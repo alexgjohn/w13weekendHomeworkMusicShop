886aed2b3550e29d2098b9929fe2851f11923c5e
Alex <alexgjohn@outlook.com>
2023-06-30 13:52:44 +0100

    completed mvp and working on extensions

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,60 @@
 package shop;
 
+import behaviours.ISell;
+import items.Item;
+import items.StringInstrument;
+
+import java.util.HashMap;
+
 public class Shop {
+
+    private HashMap<ISell, Integer> stock;
+
+    private double dailyProfits;
+
+    public Shop(){
+        this.stock = new HashMap<>();
+        this.dailyProfits = 0;
+    }
+
+    public HashMap<ISell, Integer> getStock() {
+        return stock;
+    }
+
+    public double getDailyProfits() {
+        return dailyProfits;
+    }
+
+    //this one could also take ISell as an argument.
+    public void addItemToStock(ISell item) {
+        if (!stock.containsKey(item)){
+            stock.put(item, 1);
+        } else {
+            int currentNumberInStock = stock.get(item);
+            stock.put(item, (currentNumberInStock + 1));
+        }
+    }
+
+    public String sellItem(Item item) {
+        if (!stock.containsKey(item)){
+            return String.format("The %s is not in stock.", item.getDescription());
+        } else {
+            int currentNumberInStock = stock.get(item);
+            stock.put(item, (currentNumberInStock - 1));
+            if (stock.get(item) == 0){
+                stock.remove(item);
+            }
+            dailyProfits += item.calculateMarkup();
+            return String.format("Someone bought 1 %s.", item.getDescription());
+        }
+    }
+
+    public double calculateTotalPotentialProfitForStock() {
+        double totalPotentialProfit = 0;
+        for (ISell item:getStock().keySet()){
+            double potentialProfitForItem = (item.calculateMarkup() * stock.get(item));
+            totalPotentialProfit += potentialProfitForItem;
+        }
+        return totalPotentialProfit;
+    }
 }
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,80 @@
+import items.Accessory;
+import items.StringInstrument;
+import items.WindInstrument;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import shop.Shop;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ShopTest {
+
+    private Shop shop;
+    private WindInstrument flute;
+    private StringInstrument guitar;
+    private Accessory guitarPick;
+
+    @Before
+    public void before(){
+        guitar = new StringInstrument("Acoustic guitar", 50.00, 100.00, 6);
+        flute = new WindInstrument("Irish flute", 100.00, 180.00, "Wood", 16);
+        guitarPick = new Accessory("Shiny plastic guitar pick", 0.20, 3.00);
+        shop = new Shop();
+    }
+
+    @Test
+    public void hasEmptyHashMapOfStock(){
+        assertTrue(shop.getStock().isEmpty());
+    }
+
+    //genuinely not sure why I had to structure the test this way. Should remember to ask about it in submission form.
+    @Test
+    public void canAddItemToStock() {
+        shop.addItemToStock(guitar);
+        assertEquals(1L, (long) shop.getStock().get(guitar));
+    }
+
+
+    @Test
+    public void canAddMultipleItemsToStock(){
+        shop.addItemToStock(guitar);
+        shop.addItemToStock(guitar);
+        shop.addItemToStock(guitar);
+        assertEquals(3L, (long) shop.getStock().get(guitar));
+    }
+
+    @Test
+    public void hasDailyProfits(){
+        assertEquals(0, shop.getDailyProfits(), 0.0);
+    }
+
+    @Test
+    public void canSellItem_inStock(){
+        shop.addItemToStock(guitar);
+        shop.addItemToStock(guitar);
+        shop.addItemToStock(guitar);
+        String result = shop.sellItem(guitar);
+        assertEquals(50.00, shop.getDailyProfits(), 0.0);
+        assertEquals(2L, (long) shop.getStock().get(guitar));
+        assertEquals("Someone bought 1 Acoustic guitar.", result);
+    }
+    @Test
+    public void canSellItem_notInStock(){
+        String result = shop.sellItem(guitar);
+        assertEquals(0, shop.getDailyProfits(), 0.0);
+        assertEquals("The Acoustic guitar is not in stock.", result);
+    }
+
+    @Test
+    public void canReturnTotalPotentialProfitForStock(){
+        shop.addItemToStock(guitar);
+        shop.addItemToStock(flute);
+        shop.addItemToStock(guitarPick);
+        assertEquals(132.80, shop.calculateTotalPotentialProfitForStock(), 0.0);
+    }
+
+}
